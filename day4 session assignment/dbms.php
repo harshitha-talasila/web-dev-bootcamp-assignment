41ad4adea62e2e1d47ee6130f7895864e92eee49
harshitha-talasila <85508185+harshitha-talasila@users.noreply.github.com>
2021-06-13 11:16:06 +0530

    Add files via upload

---
+++ day4 session assignment/dbms.php
@@ -0,0 +1,34 @@
+
+
+<?php 
+    $email=$_POST['emailid'];
+
+  $server="localhost:3306";
+  $dbname="mysql";
+  $username="root";
+  $password="";
+
+  $sFalg=0;
+  $conn=mysqli_connect($server,$username,$password,$dbname);
+  if(!$conn) {
+    echo 'Server Connection Failure';
+    $sFlag=0;
+  } else {
+    echo 'Server Connection Authorised';
+    $sFlag=1;
+  }
+  echo '<br/>';
+  if($sFlag==1) {
+
+    $sql="INSERT INTO email_form(email)VALUES('".$email."');";
+    $a=mysqli_query($conn,$sql);
+
+    if($a) {
+      echo "Data Inserted Successfully";
+    } else {
+      echo $conn->error;
+    }
+
+  }
+
+?>
